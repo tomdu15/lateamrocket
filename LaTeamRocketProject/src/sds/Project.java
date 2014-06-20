b0d77a3b8fdcae86c780f557d2b7847a1880fbfd
thomas.simon <thom.simon15@gmail.com>
2014-06-20 07:28:11 +0200

    COMMIT - Test project

---
+++ LaTeamRocketProject/src/sds/Project.java
@@ -0,0 +1,33 @@
+package sds;
+
+public class Project {
+
+	/**
+	 * Nom du projet
+	 */
+	private String name;
+
+	/**
+	 * Constructeur de projet
+	 * @param name le nom du projet
+	 */
+	public Project (String name){
+		this.name=name;
+	}
+	
+	/**
+	 * getter du nom
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Setter du nom
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
